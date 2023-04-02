881e3cd0bf824562eea66b0e1a2ba712f4f14ba3
André Lucas <andrelucas12L@hotmail.com>
2023-04-01 22:50:10 -0300

    primeiro commit

--- projetodjango/app/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class ToDo (models.Model):
-	atividade = models.CharField(max_length=120)
-	status = models.BooleanField(default=False)
--- projetodjango/app/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from app.serializers import ToDoSerializer
-from app.models import ToDo
-
-class ToDoViewSet(viewsets.ModelViewSet):
-    queryset = ToDo.objects.all()
-    serializer_class = ToDoSerializer
